a8baf0d2becce1e6803bab3df5a9690de455253f
Eugi <elugoc@gmail.com>
2020-01-03 03:01:35 +0000

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://admin:H6ABgkMzbmTmLD7@cluster0-yyueg.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
