1a98c78a5ee15d3fa254c845dc36327f4d5fd3c6
Eugi <elugoc@gmail.com>
2020-01-03 01:14:17 +0000

    RestServer - usuario

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 require('./config/config.js');
 
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const bodyParser = require('body-parser');
+
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -10,37 +12,20 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.get('/usuario', function(req, res) {
-    res.json('get usuario')
-})
+app.use(require('./routes/usuario.js'));
 
-app.post('/usuario', function(req, res) {
+//await 
+console.log(process.env.URLDB);
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }, (err, resp) => {
 
-    let body = req.body;
+    if (err) throw err;
 
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            obj: false,
-            mensaje: 'El nombre es necesario'
-        });
-    } else {
-        res.json({
-            persona: body
-        })
-    }
-})
+    // useNewUrlParser: true,
+    //     useUnifiedTopology: true,
+    console.log('Base de datos ONLINE');
 
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id;
+});
 
-    res.json({
-        id: id
-    })
-})
-
-app.delete('/usuario', function(req, res) {
-    res.json('delete usuario')
-})
 
 app.listen(process.env.PORT, () => {
     console.log("escuchando en el puerto:", 3000);
