de275217e2b6bfc0195aec7902dfc97b68926cf4
Eugi <elugoc@gmail.com>
2020-01-09 20:31:13 +0000

    Google sign-in

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const bodyParser = require('body-parser');
+const path = require('path');
 
 
 // parse application/x-www-form-urlencoded
@@ -11,6 +12,9 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
 app.use(bodyParser.json())
+
+//Habilitr la carpeta public
+app.use(express.static(path.resolve(__dirname, '../public')));
 
 //Configuracion de las rutas
 app.use(require('./routes/index.js'));
